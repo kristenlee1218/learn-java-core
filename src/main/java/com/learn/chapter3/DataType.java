13d45d01710232815f713fc4098cc504d3a82601
kristen <1065200748@qq.com>
2023-04-24 09:15:55 +0800

    2023.4.24

--- src/main/java/com/learn/chapter3/DataType.java
+++ src/main/java/com/learn/chapter3/DataType.java
@@ -6,7 +6,7 @@ package com.learn.chapter3;
  * @description : 《Java 核心技术卷I(原版第10版)》 第3章 3.3 数据类型
  *
  * 1、Java 是一种强类型语言
- * 这就意味着必须为每一个变量声明一种类型。在 Java 中，一共有8种基本类型（primitive type），其中有 4 种整型、
+ * 这就意味着必须为每一个变量声明一种类型。在 Java 中，一共有 8 种基本类型（primitive type），其中有 4 种整型、
  * 2 种浮点类型、1 种用于表示 Unicode 编码的字符单元的字符类型 char 和 1 种用于表示真值的 boolean 类型。
  *
  *
